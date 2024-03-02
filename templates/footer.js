ffa9442a2f2c994f0b241e5427e5332be24e95d7
rogercosta93 <rogercostacontato@gmail.com>
2024-03-02 00:38:18 -0300

    update: adicionando footer

---
+++ templates/footer.js
@@ -0,0 +1,24 @@
+
+const footer = `
+<footer>
+  <div class="container">
+    <div class="left">
+      <a href="/" class="logo">
+        R
+        <span>&lt;/&gt;</span>
+      </a>
+    </div>
+    <div class="right">
+      <span>Layout e programação por eu mesmo.</span>
+      <a href="https://instagram.com/rogercostadev">
+        <img src="/assets/icon-instagram.svg" alt="Acompanhe no Instagram" loading="lazy" decoding="async">
+      </a>
+      <a href="https://www.youtube.com/@rogercosta9370">
+        <img src="/assets/icon-youtube.svg" alt="Acompanhe no Youtube" loading="lazy" decoding="async">
+      </a>
+    </div>
+  </div>
+</footer>
+`;
+
+export default footer;
